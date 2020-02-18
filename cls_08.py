c65f0e4fdde5553b2999985ce2156fc64780a8e2
liamlycoder <luyu.real@qq.com>
2020-02-18 21:44:29 +0800

    add

--- cls_08.py
+++ cls_08.py
@@ -1,5 +1,4 @@
-# !/usr/bin/python
-# coding: utf8
+# !/usr/bin/python # coding: utf8
 # Time: 2020/1/22 15:48
 # Author: Liam
 # E-mail: luyu.real@qq.com
@@ -7,6 +6,7 @@
 import turtle
 
 
+"""
 # 上节课作业
 turtle.speed(0)
 for j in range(100):
@@ -18,7 +18,6 @@ for j in range(100):
         turtle.forward(j)
         turtle.left(90)
 turtle.done()
-"""
 turtle.speed(0)
 turtle.hideturtle()
 for c in range(3, 30):
@@ -30,5 +29,27 @@ for c in range(3, 30):
     turtle.end_fill()
 turtle.done()
 """
+"""
+# 经典鸡兔同笼
+heads = 35
+feet = 94
+for rooster in range(1, heads+1):
+    rabbit = heads - rooster
+    if 2*rooster + 4*rabbit == feet:
+        print("鸡有{}只, 兔有{}只".format(rooster, rabbit))
+"""
+
+# 升级版鸡兔同笼
+heads = int(input('请输入鸡和兔的头的总数：'))
+feet = int(input('请输入鸡和兔的脚的总数：'))
+for rooster in range(1, heads+1):
+    rabbit = heads - rooster
+    if 2*rooster + 4*rabbit == feet:
+        print("鸡有{}只, 兔有{}只".format(rooster, rabbit))
+        break
+else:
+    print('{}只头，{}只脚的情况无解'.format(heads, feet))
+
+
 
 
