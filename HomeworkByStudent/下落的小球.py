2228c5064ef3b752f1c7a74d0372d88ff2fc3e08
liamlycoder <luyu.real@qq.com>
2020-02-26 12:17:37 +0800

    add some files

--- HomeworkByStudent/下落的小球.py
+++ HomeworkByStudent/下落的小球.py
@@ -2,26 +2,35 @@ import pygame
 import sys
 import random
 
-r = radint(0, 255)
-g = radint(0, 255)
-b = radint(0, 255)
+def print_text(text, pos, color = (222, 22, 2), size = 30):
+    font1 = pygame.font.FONT(None, size)
+    img = font1.render(text, True, color)
+    c.bilt(img, pos)
 
-ball_x = 400
-ball_y = 30
-
-c = pygame.display.set_mode(800, 600)
+rect_x = 300
+rect_y = 0
+rect_w = 150
+rect_h = 50
+c = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('下落的小球')
-
+pygame.init
 while True:
     for e in pygame.event.get():
        if e.type == pygame.QUIT:
            pygame.quit()
-           eyes.exit
+           sys.exit
+
     ball_y += 1
     if ball_y > 600:
-        ball_x = radint(30, 770)
-        ball_y = 0
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        ball_x = random.randint(30, 770)
+        ball_y = 30
 
     c.fill((0, 0, 0))
     pygame.draw.circle(c, (r, g, b), (ball_x, ball_y), 30)
+    pygame.draw.rect(c, (255,0, 0), )
+    print_text('Lives:3', (0, 0))
+    print_text('Score:', (0, 0), )
     pygame.display.update
