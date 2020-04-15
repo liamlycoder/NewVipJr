4883b143db7935376e0dc0034f1d8d4d190e794e
liamlycoder <luyu.real@qq.com>
2020-04-16 01:10:42 +0800

    "modify some files"

--- Rating/cls_02.py
+++ Rating/cls_02.py
@@ -8,12 +8,13 @@ import turtle as t
 
 
 t.pensize(4)
-t.circle(100, 360)
+t.circle(100, 360)  # 绘制大圆
 
 t.penup()
 t.lt(90)
-t.fd(50)
+t.fd(50)  # 寻找新的落笔点
 
+# 绘制内圈的虚线圆
 t.pensize(1)
 t.pendown()
 t.rt(90)
@@ -35,18 +36,19 @@ t.circle(50, 45)
 t.pendown()
 t.circle(50, 22.5)
 
+# 画横线
 t.penup()
 t.goto(-120, 100)
 t.pendown()
 t.fd(240)
 
+# 画竖直线
 t.penup()
 t.goto(0, -20)
 t.pendown()
 t.lt(90)
 t.fd(240)
 
-t.hideturtle()
-
+t.hideturtle()  # 将海龟隐藏起来
 
 t.done()
