975e0b53f9e1f96a3e0df1fab448ad2153f0e32b
liamlycoder <luyu.real@qq.com>
2020-04-19 13:38:31 +0800

    补充cls2和cls3

---
+++ Rating/cls_01.py
@@ -0,0 +1,8 @@
+'''
+@Author: Liam
+@E-mail: luyu.real@qq.com
+@Date: 2020-04-19 13:37:25
+@LastEditTime: 2020-04-19 13:37:35
+@Software: Visual Studio Code
+'''
+print("Nice to meet you")
---
+++ Rating/cls_02.py
@@ -0,0 +1,11 @@
+'''
+@Author: Liam
+@E-mail: luyu.real@qq.com
+@Date: 2020-04-19 13:37:55
+@LastEditTime: 2020-04-19 13:38:15
+@Software: Visual Studio Code
+'''
+import turtle as t
+t.pensize(30)
+t.forward(200)
+t.done()
