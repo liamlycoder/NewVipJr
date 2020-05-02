88899adb0a9af1223f38ab7b38300710efb3df33
liamlycoder <luyu.real@qq.com>
2020-05-02 15:48:44 +0800

    add some files

--- AI/FaceRecognition/05人脸检测.py
+++ AI/FaceRecognition/05人脸检测.py
@@ -5,7 +5,8 @@ def face_detect_demo():
     # 将图片转换为灰度图片
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # 加载特征数据
-    faceCascade = cv.CascadeClassifier(r'D:\Python38\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
+    faceCascade = cv.CascadeClassifier(r'D:\Python38\Lib\site-packages\cv2\da'
+                                       r'ta\haarcascade_frontalface_default.xml')
     faces = faceCascade.detectMultiScale(gray)
     for x, y, w, h in faces:
         cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
@@ -17,3 +18,4 @@ img = cv.imread('lena.jpg')
 face_detect_demo()
 cv.waitKey(0)
 cv.destroyAllWindows()
+
