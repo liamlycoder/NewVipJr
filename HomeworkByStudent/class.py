f279ed7c89d6038ce99636dcf6e52320f703ed6f
liamlycoder <luyu.real@qq.com>
2020-06-06 15:18:57 +0800

    add

---
+++ HomeworkByStudent/class.py
@@ -0,0 +1,14 @@
+class Fruits:
+    def eat(self):
+        print('维生素')
+    def color(self):
+        print('color....')
+class orange(Fruits):
+    def color(self):
+        print('orange')
+class lemon(Fruits):
+    def color(self):
+        print('yellow')
+class strawberry(Fruits):
+    def color(self):
+        print('pink')
